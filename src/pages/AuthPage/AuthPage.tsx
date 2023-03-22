b16edd893e0601ddd59991741e5b02f205731a36
theflexor <nurdanokenov@gmail.com>
2023-03-22 10:58:28 +0600

    fix puck 3

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,3 +1,16 @@
+import { useState } from 'react'
+
+import { Login, Register } from '@components/index'
+
+import styles from './auth-page.module.scss'
+
 export const AuthPage = () => {
-    return <div>AuthPage</div>
+    const [isLogin, setIsLogin] = useState(false)
+    return (
+        <div className={styles.auth}>
+            <div className="container">
+                {isLogin ? <Login  /> : <Register />}
+            </div>
+        </div>
+    )
 }
