39cf49627c1643972413b503c120727b7feb8584
theflexor <nurdanokenov@gmail.com>
2023-03-22 10:58:28 +0600

    fix puck 3

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { persistor, store } from '@store/store'
 
 import { App } from './App'
 
+import './global.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
